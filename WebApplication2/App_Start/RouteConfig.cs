17f199d67c2d69f8df220a37a9d39f69f9eba727
Bruno Eduardo <94810700+BrunoEduardo45@users.noreply.github.com>
2022-11-14 16:58:19 -0300

    Adaptacao layout

---
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "clientes",
+                "clientes",
+                new {controller = "Home", action = "Clientes"}
+            );
+             routes.MapRoute(
+                "configuracao",
+                "configuracao",
+                new {controller = "Home", action = "Configuracao" }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication2/Content/Scripts/script.js
@@ -0,0 +1,10 @@
+function abrirModal(){
+    $('#modal').addClass('modal-show');             
+    $('.modal-content').addClass('modal-content-show');             
+}
+function fecharModal(){
+    $('#modal').removeClass('modal-show');
+    $('.modal-content').removeClass('modal-content-show');
+}    
+
+
