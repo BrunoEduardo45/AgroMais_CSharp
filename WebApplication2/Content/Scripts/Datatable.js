8735953adb0e73f10a853c871097bc962563c33a
Bruno Eduardo <94810700+BrunoEduardo45@users.noreply.github.com>
2022-12-07 16:54:22 -0300

    RECONSTRUCAO

--- WebApplication2/Content/Scripts/Datatable.js
+++ WebApplication2/Content/Scripts/Datatable.js
@@ -1,11 +1,3 @@
-function abrirModal(){
-    $('#modal').addClass('modal-show');             
-    $('.modal-content').addClass('modal-content-show');             
-}
-function fecharModal(){
-    $('#modal').removeClass('modal-show');
-    $('.modal-content').removeClass('modal-content-show');
-}    
 $(document).ready(function () {
     $('#datatable').DataTable();
 });
